de632752b7d5f35988bbd47200a644d3f6961320
Jose Antonio Olivari <antonio.olivari@gmail.com>
2021-05-30 11:10:23 +0200

    todo completado

--- server.js
+++ server.js
@@ -14,10 +14,41 @@ const quotes = require("./quotes.json");
 //   /quotes            - Should return all quotes (json)
 //   /quotes/random     - Should return ONE quote (json)
 app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+  response.send("Neill's Quote Server!  Ask me for all my quotes using /quotes if you want a random one use /quotes/random, or use quotes/search to find one also you can do the same with /author/search to find the one you want");
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", function (req, res) {
+  n1=quotes.map((h1)=>h1.quote)
+  res.status(200).json({Citas:n1})
+});
+
+app.get("/quotes/random", function (req, res) {
+  n2 = Math.floor(Math.random()* quotes.length)
+  n1=quotes[n2].quote
+  res.status(200).json({Cita_al_azar:n1})
+});
+
+
+app.get("/quotes/search", function (req, res) {
+  let searchQuery = req.query.term; 
+  console.log(searchQuery)
+  // level 2  filter sobre el json.quotes
+  let n2 =  quotes.filter((quote) => quote.quote.includes(searchQuery) );
+  res.status(200).json({resultado:n2})
+});
+
+app.get("/author/search", function (req, res) {
+  let searchQuery = req.query.term; 
+  console.log(searchQuery)
+  // level 2  filter sobre el json.quotes
+  let n2 =  quotes.filter((quote) => quote.author.includes(searchQuery) );
+  res.status(200).json({resultado:n2})
+});
+
+
+
+
 
 //...END OF YOUR CODE
 
@@ -25,9 +56,7 @@ app.get("/", function (request, response) {
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+
 
 //Start our server so that it listens for HTTP requests!
 let port = 5000;
